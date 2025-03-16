e22e4fe9c20cd2ac5bf1521362b7a1a12f3f074d
Howard Kao <grapekao2017@gmail.com>
2025-03-17 01:15:55 +0800

    Explore

--- src/pages/Explore/components/people.tsx
+++ src/pages/Explore/components/people.tsx
@@ -1,19 +1,16 @@
 import { useAppSelector } from "../../../redux/hooks";
 import User from "../../Home/components/Recommend/user";
-
 const People: React.FC = () => {
   const friendReducer = useAppSelector((state) => state.friendReducer);
   const friends = friendReducer.friends;
-
   return (
-    <div className="h-full flex flex-col">
-      <p className="font-bold text-[13px] p-4 h-[10%]">建議</p>
-      <div className="flex-1 overflow-y-auto ">
+    <div>
+      <p className="font-bold text-[15px] mt-10">建議</p>
+      <div>
         {friends.map((item) => {
           const { id, account, image, name, flag } = item;
           return (
             <User
-              key={id}
               id={id}
               account={account}
               image={image}
