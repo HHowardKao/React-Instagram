504787a64665dcd3537ca2ef7b474f6a35390f9b
Howard Kao <grapekao2017@gmail.com>
2025-03-16 22:28:04 +0800

    Explore

---
+++ src/pages/Explore/components/people.tsx
@@ -0,0 +1,5 @@
+const People: React.FC = () => {
+  return <h1>hello world</h1>;
+};
+
+export default People;
--- src/pages/Explore/index.tsx
+++ src/pages/Explore/index.tsx
@@ -1,5 +1,30 @@
+import Container from "../../components/Container";
+import Sidebar from "../../components/Sidebar";
+import Bottombar from "../../components/Bottombar";
+import People from "./components/people";
+import Navbar from "./components/navbar";
 const Explore: React.FC = () => {
-  return <h1>hello</h1>;
+  return (
+    <>
+      <div className="block lg:hidden w-full fixed top-0  z-50">
+        <Navbar />
+      </div>
+
+      <Container className=" pb-[35px]  pt-[56px] lg:pb-0 lg:pt-0">
+        <div className="flex  lg:justify-center">
+          <div className=" hidden lg:block lg:w-[262px] sticky top-0 h-screen">
+            <Sidebar />
+          </div>
+          <div className="w-full lg:w-[762px] overflow-y-auto">
+            <People />
+          </div>
+        </div>
+      </Container>
+      <div className="block  lg:hidden w-full fixed bottom-0  z-50">
+        <Bottombar />
+      </div>
+    </>
+  );
 };
 
 export default Explore;
