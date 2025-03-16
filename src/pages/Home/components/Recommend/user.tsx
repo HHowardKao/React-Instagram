1d4f3573a9727e3059b047373eaadd488de3ea2d
Howard Kao <grapekao2017@gmail.com>
2025-03-17 00:00:55 +0800

    Explore

--- src/pages/Home/components/Recommend/user.tsx
+++ src/pages/Home/components/Recommend/user.tsx
@@ -1,6 +1,7 @@
 import { follow, unFollow } from "../../../../redux/Friendslice";
 import { useAppDispatch } from "../../../../redux/hooks";
 import { memo } from "react";
+
 type UserProps = {
   id?: number;
   showFollow?: boolean;
@@ -24,31 +25,34 @@ const User: React.FC<UserProps> = memo(
     }
 
     return (
-      <div className="flex items-center pb-2 ml-3 mt-5 mb-5">
+      <div className="flex items-center px-4 py-2 hover:bg-gray-50">
+        {/* 頭像 */}
         <div
-          className="w-9 h-9 rounded-full mr-3 cursor-pointer"
+          className="w-9 h-9 rounded-full mr-3 cursor-pointer bg-cover bg-center"
           style={{
             backgroundImage: `url(${image})`,
-            backgroundPosition: "center",
-            backgroundSize: "cover",
           }}
         ></div>
-        <div className="block">
-          <div className="text-[11px] font-bold cursor-pointer">{account}</div>
-          <div className="text-gray-400 mt-1 text-[10px] ">{name}</div>
+
+        {/* 帳號與名稱 */}
+        <div className="flex flex-col cursor-pointer">
+          <span className="text-[11px] font-bold">{account}</span>
+          <span className="text-gray-400 text-[10px] mt-[2px]">{name}</span>
         </div>
-        <div className="flex  absolute right-0">
-          {showFollow && (
+
+        {/* 追蹤按鈕，靠右 */}
+        {showFollow && (
+          <div className="ml-auto">
             <p
               className={`${
                 flag ? "text-gray-700" : "text-blue-400"
-              } text-[12px] cursor-pointer mr-2`}
+              } text-[12px] cursor-pointer`}
               onClick={followClickHandler}
             >
               {flag ? "追蹤中" : "追蹤"}
             </p>
-          )}
-        </div>
+          </div>
+        )}
       </div>
     );
   }
