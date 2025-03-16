dbf4071907d382db2a8a67790ee61eae7e8c7671
Howard Kao <grapekao2017@gmail.com>
2025-03-17 00:46:45 +0800

    Explore

--- src/pages/Explore/index.tsx
+++ src/pages/Explore/index.tsx
@@ -35,35 +35,33 @@ const Explore: React.FC = () => {
   //   );
   return (
     <>
-      <Container className=" pb-[35px]  pt-[56px] lg:pb-0 lg:pt-0">
-        {/* 全頁固定，不滾動 */}
-        <div className="fixed inset-0 h-screen  overflow-hidden flex flex-col">
-          {/* 手機版 Navbar */}
-          <div className="block lg:hidden w-full fixed top-0 z-50">
-            <Navbar />
-          </div>
-
-          {/* 中間內容區 */}
-          <div className="flex flex-1 pt-[56px] lg:pt-0">
-            {/* 左邊 Sidebar，固定不動 */}
-            <div className="hidden lg:block fixed top-0 left-0 w-[262px] h-screen z-40">
-              <Sidebar />
-            </div>
-
-            {/* 右側 People 區塊 */}
-            <div className="w-full lg:ml-[262px] lg:w-[calc(100%-262px)] flex justify-center mt-10">
-              <div className="w-full max-w-[500px] h-[calc(100vh-80px)] overflow-y-auto">
-                <People />
-              </div>
-            </div>
-          </div>
-
-          {/* 手機版底部 Bottombar */}
-          <div className="block lg:hidden w-full fixed bottom-0 z-50">
-            <Bottombar />
-          </div>
+      {/* 全頁固定，高度限制、禁止頁面滾動 */}
+      <div className="h-screen overflow-hidden flex flex-col">
+        {/* 手機 Navbar */}
+        <div className="block lg:hidden w-full fixed top-0 z-50">
+          <Navbar />
         </div>
-      </Container>
+
+        {/* Container 包裹整體內容 */}
+        <Container className="flex flex-1 pt-[56px] lg:pt-0">
+          {/* 左側 Sidebar 在 Container 內，高度固定 */}
+          <div className="hidden lg:block sticky top-0 h-screen w-[262px]">
+            <Sidebar />
+          </div>
+
+          {/* 右側主內容滾動 */}
+          <div className="w-full lg:w-[calc(100%-262px)] flex justify-center mt-10">
+            <div className="w-full max-w-[500px] h-[calc(100vh-80px)] overflow-y-auto">
+              <People />
+            </div>
+          </div>
+        </Container>
+
+        {/* 手機底部 Bottombar */}
+        <div className="block lg:hidden w-full fixed bottom-0 z-50">
+          <Bottombar />
+        </div>
+      </div>
     </>
   );
 };
