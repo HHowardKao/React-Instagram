e9d059f0ba2ceedea6d24a56171d41faa0d4ab09
Howard Kao <grapekao2017@gmail.com>
2025-03-16 23:43:45 +0800

    Explore

--- src/pages/Explore/index.tsx
+++ src/pages/Explore/index.tsx
@@ -3,36 +3,27 @@ import Sidebar from "../../components/Sidebar";
 import Bottombar from "../../components/Bottombar";
 import Navbar from "./components/navbar";
 import People from "./components/people";
-
 const Explore: React.FC = () => {
   return (
     <>
-      {/* Navbar for small screens */}
-      <div className="block lg:hidden w-full fixed top-0 z-50">
+      <div className="block lg:hidden w-full fixed top-0  z-50">
         <Navbar />
       </div>
 
-      {/* Main Content */}
-      <Container className="pt-[56px] pb-[35px] lg:pt-0 lg:pb-0">
-        <div className="flex h-screen">
-          {/* Sidebar - Fixed on large screens */}
-          <div className="hidden lg:block lg:w-[262px]">
-            <div className="sticky top-0 h-screen">
-              <Sidebar />
-            </div>
+      <Container className=" pb-[35px]  pt-[56px] lg:pb-0 lg:pt-0">
+        <div className="flex  lg:justify-center">
+          <div className=" hidden lg:block lg:w-[262px] sticky top-0 h-screen">
+            <Sidebar />
           </div>
-
-          {/* People - Scrollable Content */}
           <div className="w-full lg:w-[500px] overflow-y-auto">
-            <div className="mt-10 lg:mt-0 px-4">
-              <People />
-            </div>
+            <People />
+          </div>
+          <div className="w-full lg:block lg:w-[262px] sticky top-0 h-screen mt-10">
+            <People />
           </div>
         </div>
       </Container>
-
-      {/* Bottombar for small screens */}
-      <div className="block lg:hidden w-full fixed bottom-0 z-50">
+      <div className="block  lg:hidden w-full fixed bottom-0  z-50">
         <Bottombar />
       </div>
     </>
