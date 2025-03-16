b0e67669e107648f5a2ecd7d044a53f9dde2c171
Howard Kao <grapekao2017@gmail.com>
2025-03-17 01:50:24 +0800

    Explore

--- src/pages/Explore/components/people.tsx
+++ src/pages/Explore/components/people.tsx
@@ -5,7 +5,7 @@ const People: React.FC = () => {
   const friends = friendReducer.friends;
   return (
     <div>
-      <div className="border-box h-[60px] bg-white flex items-center justify-between lg:mt-15  ml-5 lg:mb-5 ">
+      <div className="border-box h-[60px] bg-white flex items-center justify-between lg:mt-10  ml-5 lg:mb-5 ">
         <p className="font-bold text-[15px] ">建議</p>
       </div>
 
