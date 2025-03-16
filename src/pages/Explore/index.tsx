c19bb11bd06abb0706de902c38a027dd4ed731b6
Howard Kao <grapekao2017@gmail.com>
2025-03-16 23:13:43 +0800

    Explore

--- src/pages/Explore/index.tsx
+++ src/pages/Explore/index.tsx
@@ -15,7 +15,10 @@ const Explore: React.FC = () => {
           <div className=" hidden lg:block lg:w-[262px] sticky top-0 h-screen">
             <Sidebar />
           </div>
-          <div className="w-full lg:w-[762px] overflow-y-auto ">
+          <div className="w-full lg:w-[500px] overflow-y-auto">
+            <People />
+          </div>
+          <div className="hidden lg:block lg:w-[262px] sticky top-0 h-screen">
             <People />
           </div>
         </div>
